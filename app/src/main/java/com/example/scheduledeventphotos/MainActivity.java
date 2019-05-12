0930149806c03aeeda62065265a05cabcdf227bc
gcsimon <gio.simon@hotmail.com>
2019-05-11 23:56:38 -0300

    main activity and add event activity layout

--- app/src/main/java/com/example/scheduledeventphotos/MainActivity.java
+++ app/src/main/java/com/example/scheduledeventphotos/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.scheduledeventphotos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button5);
+
+        btn.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View v){
+                Intent myIntent = new Intent(MainActivity.this, AddEventActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
+
+
+
 }
