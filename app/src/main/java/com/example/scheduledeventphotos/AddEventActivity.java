19dddff6acbfadcb61ee02b498d126881b5026ab
gcsimon <gio.simon@hotmail.com>
2019-06-03 23:31:56 -0300

    fixed input types

--- app/src/main/java/com/example/scheduledeventphotos/AddEventActivity.java
+++ app/src/main/java/com/example/scheduledeventphotos/AddEventActivity.java
@@ -1,7 +1,10 @@
 package com.example.scheduledeventphotos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEventActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class AddEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+
+        Button btn1 = (Button) findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AddEventActivity.this, MainActivity.class);
+                AddEventActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
+
 }
